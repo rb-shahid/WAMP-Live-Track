52fac5534ab680089a17a1a3fd4f9ba83b983c7f
shahid ghafoor <shahidghafoor00@gmail.com>
2018-12-15 14:50:27 +0500

    add library and get location from server

--- app/src/main/java/com/byteshaft/wamplivetrack/MainActivity.java
+++ app/src/main/java/com/byteshaft/wamplivetrack/MainActivity.java
@@ -1,23 +1,18 @@
 package com.byteshaft.wamplivetrack;
 
 import android.Manifest;
-import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.location.Location;
-import android.location.LocationManager;
-import android.support.annotation.ColorInt;
 import android.support.annotation.DrawableRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.content.res.ResourcesCompat;
-import android.support.v4.graphics.drawable.DrawableCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.WindowManager;
@@ -26,12 +21,10 @@ import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -51,11 +44,6 @@ import static android.content.pm.PackageManager.PERMISSION_GRANTED;
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback,
         GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
-    private static final int LOCATION_PERM_REQ_CODE = 1;
-    private FusedLocationProviderClient mFusedLocationClient;
-    private LocationRequest mLocationRequest;
-    private LocationCallback mLocationCallback;
-
     private Session mWAMPSession;
     private TextView mStatusText;
     private TextView mSpeedText;
@@ -63,9 +51,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     GoogleApiClient mGoogleApiClient;
 
     private GoogleMap mMap;
-    private double mLatitude;
-    private double mLongitude;
-    private float mSpeed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -79,16 +64,11 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mapFragment.getMapAsync(this);
         mStatusText = findViewById(R.id.status_text);
         mSpeedText = findViewById(R.id.speed_text);
-
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-        createLocationCallback();
-        createLocationRequest();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        checkLocationPersmissionIfNotPresentThenRequest();
         connectToServerAndPublishLocation();
     }
 
@@ -104,17 +84,10 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mWAMPSession.addOnJoinListener((session, details) -> {
             mStatusText.setText("Status: " + "Connected");
             mStatusText.setTextColor(Color.GREEN);
-            startLocationUpdates();
+            getLocationFromServer();
         });
         Client wampClient = new Client(mWAMPSession, "ws://51.15.107.119:8080/ws", "realm1");
         wampClient.connect().whenComplete((exitInfo, throwable) -> stopLocationUpdates());
-    }
-
-    private void createLocationRequest() {
-        mLocationRequest = new LocationRequest();
-        mLocationRequest.setInterval(500);
-        mLocationRequest.setFastestInterval(500);
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
     }
 
     private BitmapDescriptor vectorToBitmap(@DrawableRes int id) {
@@ -127,92 +100,26 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         return BitmapDescriptorFactory.fromBitmap(bitmap);
     }
 
-    private void createLocationCallback() {
-        mLocationCallback = new LocationCallback() {
-            @Override
-            public void onLocationResult(LocationResult locationResult) {
-                super.onLocationResult(locationResult);
-                Location location = locationResult.getLastLocation();
-                if (location != null) {
-                    mLatitude = location.getLongitude();
-                    mLongitude = location.getLatitude();
-                    mSpeed = location.getSpeed();
-                    mWAMPSession.publish("io.crossbar.location", mLatitude, mLongitude, mSpeed);
-                    mSpeedText.setText("Speed: " + mSpeed);
-                    System.out.println(mLatitude + "  " + mLongitude + "speed " + mSpeed);
-
-                    LatLng current = new LatLng(location.getLatitude(), location.getLongitude());
-                    mMap.addMarker(new MarkerOptions()
-                            .icon(vectorToBitmap(R.drawable.bike))
-                            .position(current).title("Marker Label")
-                            .snippet("Marker Description"));
-                    CameraPosition cameraPosition = new CameraPosition.Builder().target(current).zoom(30).build();
-                    mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-                }
-            }
-        };
-    }
-
-    private void startLocationUpdates() {
-        if (ContextCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) != PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{ACCESS_FINE_LOCATION},
-                    LOCATION_PERM_REQ_CODE);
-        } else {
-            mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, null);
-        }
+    private void getLocationFromServer() {
+        mWAMPSession.subscribe("io.crossbar.location", (o, eventDetails) -> {
+            System.out.println(o);
+            
+            LatLng current = new LatLng((double) o.get(1), (double) o.get(0));
+            mSpeedText.setText(String.format("Speed: %s", o.get(2)));
+            mMap.addMarker(new MarkerOptions()
+                    .icon(vectorToBitmap(R.drawable.bike))
+                    .position(current).title("Marker Label")
+                    .snippet("Marker Description"));
+            CameraPosition cameraPosition = new CameraPosition.Builder().target(current).zoom(14).build();
+            mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+        });
     }
 
     private void stopLocationUpdates() {
         if (mWAMPSession.isConnected()) {
             mWAMPSession.leave();
         }
-        mFusedLocationClient.removeLocationUpdates(mLocationCallback);
         mStatusText.setText("Disconnected");
-    }
-
-    private void checkLocationPersmissionIfNotPresentThenRequest() {
-        if (ContextCompat.checkSelfPermission(MainActivity.this,
-                Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-            // Permission is not granted
-
-            ActivityCompat.requestPermissions(MainActivity.this,
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                    1);
-        } else {
-            // permission granted
-            Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
-            // TODO: 10/12/2018 do location related work
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
-                                           @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case 1:
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    if (ActivityCompat.checkSelfPermission(
-                            this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                            && ActivityCompat.checkSelfPermission(this,
-                            Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                        // TODO: Consider calling
-                        //    ActivityCompat#requestPermissions
-                        // here to request the missing permissions, and then overriding
-                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                        //                                          int[] grantResults)
-                        // to handle the case where the user grants the permission. See the documentation
-                        // for ActivityCompat#requestPermissions for more details.
-                        return;
-                    }
-                    startLocationUpdates();
-
-                } else {
-                    // if permission not granted, it will close the app
-                    System.exit(0);
-                }
-        }
     }
 
     protected synchronized void buildGoogleApiClient() {
@@ -243,14 +150,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
     @Override
-    public void onConnected(@Nullable Bundle bundle) {
-        mLocationRequest = new LocationRequest();
-        mLocationRequest.setInterval(1000);
-        mLocationRequest.setFastestInterval(1000);
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
-    }
-
-    @Override
     public void onConnectionSuspended(int i) {
 
     }
@@ -259,4 +158,10 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
     }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
 }
